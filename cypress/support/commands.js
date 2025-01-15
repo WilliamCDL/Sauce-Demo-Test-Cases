2ab0076e31bc506b1d93832d3a2b9c62d685c325
WilliamCDL <will.correia.lima@gmail.com>
2025-01-14 22:18:15 -0300

    3 e 4: adição de todos os testes de adição e remoção do carrinho
    Adicionados testes de adição ao carrinho via listagem ou pagina após seleção de produto, Adicionada remoção via listagem, após seleção de produto e via carrinho

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,6 +1,8 @@
 import loginPageActions from "../../PageObjects/PageActions/LoginPageActions"
 import inventoryPageActions from "../../PageObjects/PageActions/InventoryPageActions"
 import productPageActions from "../../PageObjects/PageActions/ItemPageActions"
+import itemPageActions from "../../PageObjects/PageActions/ItemPageActions";
+import cartPageActions from "../../PageObjects/PageActions/CartPageActions.js"
 //teste de login
 Cypress.Commands.add('loginTest', (name, username, password, expected) => {
     //para testes com login ou senha vazios faz-se necessario uma verificação para não executar comandos pois 
@@ -54,5 +56,57 @@ Cypress.Commands.add('productChecking', (name, description, price, butonName, im
     productPageActions.checkProducPrice(price)
     productPageActions.checkProducButton(butonName)
     productPageActions.checkProducImage(imageName)
+    return
+});
+
+Cypress.Commands.add('addToCartFromList', (index, count) => {
+    inventoryPageActions.clickButton(0)
+    inventoryPageActions.cartCount(count)
+    inventoryPageActions.checkProducButton2(index, "REMOVE")
+    return
+});
+
+Cypress.Commands.add('removeCartFromList', (index, count) => {
+    inventoryPageActions.clickButton2(0)
+    if (count === 0) {
+    } else {
+        inventoryPageActions.cartCount(count)
+    }
+    inventoryPageActions.checkProducButton(index, "ADD TO CART")
+    return
+});
+
+Cypress.Commands.add('addProduct', (name, count) => {
+    itemPageActions.clickButton(0)
+    itemPageActions.cartCount(count)
+    itemPageActions.checkProducName(name)
+    itemPageActions.checkProducButton2("REMOVE")
+    return
+});
+
+Cypress.Commands.add('removeProduct', (name, count) => {
+    itemPageActions.clickButton2(0)
+    if (count === 0) {
+    } else {
+        itemPageActions.cartCount(count)
+    }
+    itemPageActions.checkProducName(name)
+    itemPageActions.checkProducButton("ADD TO CART")
+    return
+});
+
+Cypress.Commands.add('clickNameProduct', (index) => {
+    inventoryPageActions.clickNameItem(index)
+    return
+});
+
+Cypress.Commands.add('removeFromCart', (name, count) => {
+    cartPageActions.checkProducName(0, name)
+    cartPageActions.clickButton2(0)
+    if (count === 0) {
+    } else {
+        cartPageActions.cartCount(count)
+    }
+
     return
 });
